ef7ac587c904a150432d3343c61c65af475a3679
Robert M. Koch <threadmetal@gmail.com>
2012-02-18 03:39:53 -0500

    Attempting Gmail

--- config/initializers/mailer_config.rb
+++ config/initializers/mailer_config.rb
@@ -1,2 +1,11 @@
 
-ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:domain => "threadmetal.net",
+	:user_name => ENV["GMAIL_USER"],
+	:password => ENV["GMAIL_PASSWORD"],
+	:authentication => 'plain',
+	:enable_starttls_auto => true
+}
