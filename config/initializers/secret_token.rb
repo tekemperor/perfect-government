e9f0c2c1fef646f67dbd72ef4fb2b4ca520484d8
Robert M. Koch <threadmetal@gmail.com>
2012-02-17 21:44:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PerfectGovernment::Application.config.secret_token = 'e48f29abe445b95c1246fa321829112a5ef4991e45bd5742f0149d1fbee599ee3e3b1fb34776cbe699b493ec71b0553cd0cba74759d773e943160def141ab3ba'
